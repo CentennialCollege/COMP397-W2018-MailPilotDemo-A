3a1a9e723b859571da49f3e68e56f4110dd8bbb1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-02-17 15:03:24 -0500

    added plane object

--- Scripts/scenes/play.ts
+++ Scripts/scenes/play.ts
@@ -2,6 +2,7 @@ module scenes {
   export class PlayScene extends objects.Scene {
     // Private Instance Variables
     private _ocean: objects.Ocean;
+    private _plane: objects.Plane;
 
     // Public Properties
 
@@ -21,17 +22,22 @@ module scenes {
     // Initialize Game Variables and objects
     public Start(): void {
       this._ocean = new objects.Ocean(this.assetManager);
+      this._plane = new objects.Plane(this.assetManager);
       this.Main();
     }
 
     public Update(): void {
       this._ocean.Update();
+      this._plane.Update();
     }
 
     // This is where the fun happens
     public Main(): void {
       // add ocean to the scene
       this.addChild(this._ocean);
+
+      // add plane to this scene
+      this.addChild(this._plane);
     }
   }
 }
