35d30b441c50152defdb8c81c4c3a3a2ca7979d9
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-02-24 14:11:54 -0500

    added sounds

--- Scripts/managers/collision.js
+++ Scripts/managers/collision.js
@@ -10,8 +10,15 @@ var managers;
             // check if there is a collision
             if (math.Vec2.Distance(P1, P2) < (object1.halfHeight + object2.halfHeight)) {
                 if (!object2.isColliding) {
-                    console.log("collision with " + object2.name);
                     object2.isColliding = true;
+                    switch (object2.name) {
+                        case "island":
+                            createjs.Sound.play("yay");
+                            break;
+                        case "cloud":
+                            createjs.Sound.play("thunder");
+                            break;
+                    }
                 }
             }
             else {
